a6daf70fa18de73935a0c3b9a8d43ed4195a1ffa
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-03 18:09:04 -0400

    feat: wire real Supabase identity into ATLAS

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { AtlasProvider } from './app/AtlasContext';
+import { createAtlasSupabaseClient } from './lib/supabase/client';
+import { createAtlasIdentitySource } from './lib/supabase/atlasIdentitySource';
 import './styles.css';
+
+const identitySource = createAtlasIdentitySource(createAtlasSupabaseClient());
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AtlasProvider source={identitySource}>
+        <App />
+      </AtlasProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
