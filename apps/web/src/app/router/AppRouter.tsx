8391782b70745aef3c0348104634795a928c8d8d
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-04 05:07:55 -0400

    feat: route Bank Cash reconciliation modules

--- apps/web/src/app/router/AppRouter.tsx
+++ apps/web/src/app/router/AppRouter.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { AtlasShell } from '../AtlasShell';
 import { RouteErrorPage } from '../errors/RouteErrorPage';
 import { AccountingPage } from '../../modules/accounting/AccountingPage';
+import { BankCashPage } from '../../modules/accounting/BankCashPage';
 import { ChartOfAccountsPage } from '../../modules/accounting/ChartOfAccountsPage';
 import { GeneralLedgerPage } from '../../modules/accounting/GeneralLedgerPage';
 import { JournalEntriesPage } from '../../modules/accounting/JournalEntriesPage';
 import { PayablesPage } from '../../modules/accounting/PayablesPage';
 import { ReceivablesPage } from '../../modules/accounting/ReceivablesPage';
+import { ReconciliationPage } from '../../modules/accounting/ReconciliationPage';
 import { FinanceHome } from '../../modules/finance/FinanceHome';
 import { HealthRoutes } from '../../modules/health/HealthRoutes';
 import { EnterpriseHome } from '../../modules/home/EnterpriseHome';
@@ -23,6 +25,8 @@ export function AppRouter() {
         <Route path="/finance/accounting/journal-entries" element={<JournalEntriesPage />} />
         <Route path="/finance/accounting/accounts-receivable" element={<ReceivablesPage />} />
         <Route path="/finance/accounting/accounts-payable" element={<PayablesPage />} />
+        <Route path="/finance/accounting/bank-cash" element={<BankCashPage />} />
+        <Route path="/finance/accounting/reconciliation" element={<ReconciliationPage />} />
         <Route path="/health/*" element={<HealthRoutes />} />
       </Route>
       <Route path="*" element={<RouteErrorPage />} />
