a26ab6634fec40321bf707ed6d262a0cc6506cce
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-04 03:18:39 -0400

    feat: route real AR AP pages

--- apps/web/src/app/router/AppRouter.tsx
+++ apps/web/src/app/router/AppRouter.tsx
@@ -5,6 +5,8 @@ import { AccountingPage } from '../../modules/accounting/AccountingPage';
 import { ChartOfAccountsPage } from '../../modules/accounting/ChartOfAccountsPage';
 import { GeneralLedgerPage } from '../../modules/accounting/GeneralLedgerPage';
 import { JournalEntriesPage } from '../../modules/accounting/JournalEntriesPage';
+import { PayablesPage } from '../../modules/accounting/PayablesPage';
+import { ReceivablesPage } from '../../modules/accounting/ReceivablesPage';
 import { FinanceHome } from '../../modules/finance/FinanceHome';
 import { HealthRoutes } from '../../modules/health/HealthRoutes';
 import { EnterpriseHome } from '../../modules/home/EnterpriseHome';
@@ -19,6 +21,8 @@ export function AppRouter() {
         <Route path="/finance/accounting/general-ledger" element={<GeneralLedgerPage />} />
         <Route path="/finance/accounting/chart-of-accounts" element={<ChartOfAccountsPage />} />
         <Route path="/finance/accounting/journal-entries" element={<JournalEntriesPage />} />
+        <Route path="/finance/accounting/accounts-receivable" element={<ReceivablesPage />} />
+        <Route path="/finance/accounting/accounts-payable" element={<PayablesPage />} />
         <Route path="/health/*" element={<HealthRoutes />} />
       </Route>
       <Route path="*" element={<RouteErrorPage />} />
