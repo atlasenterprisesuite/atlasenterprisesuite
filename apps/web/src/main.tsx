78edb6c3ce6cd448e3d9ce7af8f16bb1bb3b578c
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-03 20:23:55 -0400

    feat: wire real Chart account write service

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import {
+  AccountWriteService,
   createSupabaseAccountingRepository,
   JournalWriteService,
   SupabaseAccountingWriteGateway,
+  SupabaseAccountWriteGateway,
 } from '../../../packages/accounting/src';
 import { App } from './App';
 import { AtlasProvider } from './app/AtlasContext';
+import { AccountWriteProvider } from './modules/accounting/AccountWriteProvider';
 import { AccountingRepositoryProvider } from './modules/accounting/AccountingDataProvider';
 import { AccountingWriteProvider } from './modules/accounting/AccountingWriteProvider';
 import { createAtlasSupabaseClient } from './lib/supabase/client';
@@ -22,6 +25,9 @@ const accountingRepository = supabaseClient
 const accountingWriteService = supabaseClient
   ? new JournalWriteService(new SupabaseAccountingWriteGateway(supabaseClient))
   : null;
+const accountWriteService = supabaseClient
+  ? new AccountWriteService(new SupabaseAccountWriteGateway(supabaseClient))
+  : null;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -29,7 +35,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <AtlasProvider source={identitySource}>
         <AccountingRepositoryProvider repository={accountingRepository}>
           <AccountingWriteProvider service={accountingWriteService}>
-            <App />
+            <AccountWriteProvider service={accountWriteService}>
+              <App />
+            </AccountWriteProvider>
           </AccountingWriteProvider>
         </AccountingRepositoryProvider>
       </AtlasProvider>
