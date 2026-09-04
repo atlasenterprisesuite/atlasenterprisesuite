96bbeeda7da00efed7848ca53666b0a934f68fb2
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-03 20:15:34 -0400

    feat: route ledger and Chart of Accounts

--- apps/web/src/app/router/AppRouter.tsx
+++ apps/web/src/app/router/AppRouter.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import { AtlasShell } from '../AtlasShell';
 import { RouteErrorPage } from '../errors/RouteErrorPage';
 import { AccountingPage } from '../../modules/accounting/AccountingPage';
+import { ChartOfAccountsPage } from '../../modules/accounting/ChartOfAccountsPage';
+import { GeneralLedgerPage } from '../../modules/accounting/GeneralLedgerPage';
 import { JournalEntriesPage } from '../../modules/accounting/JournalEntriesPage';
 import { FinanceHome } from '../../modules/finance/FinanceHome';
 import { HealthRoutes } from '../../modules/health/HealthRoutes';
@@ -14,6 +16,8 @@ export function AppRouter() {
         <Route path="/" element={<EnterpriseHome />} />
         <Route path="/finance" element={<FinanceHome />} />
         <Route path="/finance/accounting" element={<AccountingPage />} />
+        <Route path="/finance/accounting/general-ledger" element={<GeneralLedgerPage />} />
+        <Route path="/finance/accounting/chart-of-accounts" element={<ChartOfAccountsPage />} />
         <Route path="/finance/accounting/journal-entries" element={<JournalEntriesPage />} />
         <Route path="/health/*" element={<HealthRoutes />} />
       </Route>
