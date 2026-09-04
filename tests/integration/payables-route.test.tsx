867f372667931a89ae129c253d8537c701a6532d
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-04 13:47:19 -0400

    test: run Accounts Payable route integration in jsdom

--- tests/integration/payables-route.test.tsx
+++ tests/integration/payables-route.test.tsx
@@ -1,3 +1,5 @@
+// @vitest-environment jsdom
+import '@testing-library/jest-dom/vitest';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
