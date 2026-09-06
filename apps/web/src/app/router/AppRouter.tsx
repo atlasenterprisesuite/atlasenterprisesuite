c929407c53c5da4c39c39c8f96c584b460d5dfe9
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-06 03:14:54 -0400

    fix: enforce Telecom permission gate at the route

--- apps/web/src/app/router/AppRouter.tsx
+++ apps/web/src/app/router/AppRouter.tsx
@@ -12,7 +12,7 @@ import { ReconciliationPage } from '../../modules/accounting/ReconciliationPage'
 import { FinanceHome } from '../../modules/finance/FinanceHome';
 import { HealthRoutes } from '../../modules/health/HealthRoutes';
 import { EnterpriseHome } from '../../modules/home/EnterpriseHome';
-import { MifiControlPage } from '../../modules/telecom/MifiControlPage';
+import { TelecomMifiRoute } from '../../modules/telecom/TelecomMifiRoute';
 import { SpatialEntry } from '../../spatial/SpatialEntry';
 
 export function AppRouter() {
@@ -31,7 +31,7 @@ export function AppRouter() {
         <Route path="/finance/accounting/bank-cash" element={<BankCashPage />} />
         <Route path="/finance/accounting/reconciliation" element={<ReconciliationPage />} />
         <Route path="/health/*" element={<HealthRoutes />} />
-        <Route path="/telecom/devices/mifi" element={<MifiControlPage />} />
+        <Route path="/telecom/devices/mifi" element={<TelecomMifiRoute />} />
       </Route>
       <Route path="*" element={<RouteErrorPage />} />
     </Routes>
