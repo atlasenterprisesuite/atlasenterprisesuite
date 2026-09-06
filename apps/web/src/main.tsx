912fd2a3c535c134b613b624fc4e598ea956d47e
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-06 09:54:32 -0400

    feat(voice): expose ATLAS Voice route

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { AtlasShell } from './components/AtlasShell';
+import { AtlasVoicePage } from './modules/voice/AtlasVoicePage';
 import './styles.css';
 import './health.css';
+
+function Root() {
+  const isVoiceRoute = window.location.pathname === '/voice';
+  if (isVoiceRoute) {
+    return (
+      <AtlasShell>
+        <AtlasVoicePage />
+      </AtlasShell>
+    );
+  }
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Root />
     </BrowserRouter>
   </React.StrictMode>
 );
