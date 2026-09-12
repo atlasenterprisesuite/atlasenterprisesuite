ca6f1ae901909ea0c24130f64c839f648bcbe109
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-12 09:41:30 -0400

    feat(ride): add protected Ride route graph

---
+++ apps/web/src/modules/ride/RideRoutes.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import { AtlasShell } from '../../components/AtlasShell';
+import { RequireAtlasIdentity } from '../../identity/RequireAtlasIdentity';
+import { ComplianceHomePage } from './ComplianceHomePage';
+import { DocumentsPage } from './DocumentsPage';
+import { DriverHomePage } from './DriverHomePage';
+import { ProfilePhotoCompliancePage } from './ProfilePhotoCompliancePage';
+import { RideHomePage } from './RideHomePage';
+
+export function RideRoutes() {
+  return (
+    <AtlasShell>
+      <RequireAtlasIdentity>
+        <Routes>
+          <Route path="/ride" element={<RideHomePage />} />
+          <Route path="/ride/driver" element={<DriverHomePage />} />
+          <Route path="/ride/driver/compliance" element={<ComplianceHomePage />} />
+          <Route path="/ride/driver/compliance/documents" element={<DocumentsPage />} />
+          <Route path="/ride/driver/compliance/documents/profile-photo" element={<ProfilePhotoCompliancePage />} />
+        </Routes>
+      </RequireAtlasIdentity>
+    </AtlasShell>
+  );
+}
