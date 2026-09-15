8f1b62f5b36acb61143d724691beeca0168538a0
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-15 12:54:55 -0400

    feat: add protected ATLAS Insurance route graph

---
+++ apps/web/src/modules/insurance/InsuranceRoutes.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { AtlasShell } from '../../components/AtlasShell';
+import { RequireAtlasIdentity } from '../../identity/RequireAtlasIdentity';
+import { InsuranceHome } from './InsuranceHome';
+
+export function InsuranceRoutes() {
+  return (
+    <AtlasShell>
+      <RequireAtlasIdentity>
+        <Routes>
+          <Route path="/insurance" element={<InsuranceHome />} />
+          <Route path="/insurance/verify" element={<InsuranceHome />} />
+        </Routes>
+      </RequireAtlasIdentity>
+    </AtlasShell>
+  );
+}
