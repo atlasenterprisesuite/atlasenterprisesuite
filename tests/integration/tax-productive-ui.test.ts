c3aea6e3d461ee1bbcbdd55f1e869784d11a2bd4
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-21 16:13:56 -0400

    test(tax): cover atomic source mapping import

--- tests/integration/tax-productive-ui.test.ts
+++ tests/integration/tax-productive-ui.test.ts
@@ -33,6 +33,7 @@ describe('ATLAS Tax productive UI contracts', () => {
       'tax_create_return',
       'tax_set_step_state',
       'tax_register_source_document',
+      'tax_import_source_mapping',
       'tax_record_fact',
       'tax_record_line_mapping',
       'tax_upsert_workpaper',
