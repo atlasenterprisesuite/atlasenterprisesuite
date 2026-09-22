6efc258428f5b2a149508f075f7cadb8182af915
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-21 21:21:56 -0400

    feat(tax): port Social Security Schedule D and line 16 engines onto main

--- packages/tax-forms/src/index.ts
+++ packages/tax-forms/src/index.ts
@@ -216,3 +216,7 @@ export * from './depthSourceDocuments';
 export * from './individual1040Engine';
 
 export * from './scheduleA2025';
+
+export * from './socialSecurity2025';
+export * from './scheduleD2025';
+export * from './line16Method2025';
