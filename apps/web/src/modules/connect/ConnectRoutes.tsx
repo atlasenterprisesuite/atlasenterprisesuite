b9bf1e00bf8af6aebb50d99e39c8a3d34d328841
Atlas Enterprise Suite <atlashealthfrontiers@gmail.com>
2026-09-25 03:07:27 -0400

    feat(chat): reconcile apps/web/src/modules/connect/ConnectRoutes.tsx

--- apps/web/src/modules/connect/ConnectRoutes.tsx
+++ apps/web/src/modules/connect/ConnectRoutes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { AtlasBroadcastChannelPage } from './AtlasBroadcastChannelPage';
+import { AtlasChatPage } from './AtlasChatPage';
 import { AtlasWirelessPage } from './AtlasWirelessPage';
 import { ConnectHomePage } from './ConnectHomePage';
 import { GoogleFiWirelessPage } from './GoogleFiWirelessPage';
@@ -9,6 +10,7 @@ export function ConnectRoutes() {
     <Routes>
       <Route path="/connect" element={<ConnectHomePage />} />
       <Route path="/connect/channel" element={<AtlasBroadcastChannelPage />} />
+      <Route path="/connect/chat" element={<AtlasChatPage />} />
       <Route path="/connect/wireless" element={<AtlasWirelessPage />} />
       <Route path="/connect/google-fi" element={<GoogleFiWirelessPage />} />
     </Routes>
